37f25bfb651fcd009c42cd90a16f9efb456393f3
Daniel <dabergshoeff@gmail.com>
2020-02-28 14:55:26 +0100

    Eating added

--- Assets/Scripts/SmallObject.cs
+++ Assets/Scripts/SmallObject.cs
@@ -6,11 +6,18 @@ public class SmallObject : MonoBehaviour {
     public bool HeldInHand = false;
 
     public void LetGo() {
+        CheckForEating();
         HeldInHand = true;
         Invoke("SetHeldInHand", 3f);
     }
 
     public void SetHeldInHand() {
         HeldInHand = false;
+    }
+
+    private void CheckForEating() {
+        if(Vector3.Distance(transform.position, Camera.main.transform.position) < 0.2f) {
+            Destroy(gameObject);
+        }
     }
 }
