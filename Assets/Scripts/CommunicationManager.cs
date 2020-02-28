4de4f824664b0e9a0970c3c235be04d24f40c8c8
Daniel <dabergshoeff@gmail.com>
2020-02-28 15:14:32 +0100

    Added TMPro

---
+++ Assets/Scripts/CommunicationManager.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CommunicationManager : MonoBehaviour
+{
+    public static CommunicationManager Instance { get; private set; }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Instance = this;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void DisplayMessage(GameObject go, string message) {
+
+    }
+}
