4a612e9212bfbca97052abde06795f685fc524be
Daniel <dabergshoeff@gmail.com>
2020-03-03 11:48:03 +0100

    Added songs

---
+++ Assets/Scripts/Seashell.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Seashell : MonoBehaviour
+{
+    private bool activated = false;
+    private AudioSource myAudioSource;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        myAudioSource = gameObject.AddComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other) {
+        if (!other.CompareTag("MainCamera"))
+            return;
+
+        activated = !activated;
+    }
+}
