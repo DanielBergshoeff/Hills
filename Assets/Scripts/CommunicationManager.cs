07fff03a0625d28bc426100571740be8f7b9136b
Daniel <dabergshoeff@gmail.com>
2020-03-03 10:17:58 +0100

    Messaging system additions

--- Assets/Scripts/CommunicationManager.cs
+++ Assets/Scripts/CommunicationManager.cs
@@ -8,18 +8,29 @@ public class CommunicationManager : MonoBehaviour
     public GameObject MessagePrefab;
     public GameObject Sapling;
     public float DisappearTime = 1f;
+    public LayerMask TerrainLayer;
 
     // Start is called before the first frame update
     void Start()
     {
         Instance = this;
-        DisplayMessage(Sapling, "Welcome.. Please take a deep breath..", 10f, Vector3.up * 2f);
+        DisplayMessage(Sapling, "Welcome.. Please take a deep breath..", 0f, Vector3.up * 2f, 1f);
+        //DisplayMessage(null, "Try to squeeze your hands..", 30f, Camera.main.transform.forward * 10f, 0.3f);
     }
 
-    public void DisplayMessage(GameObject go, string message, float time = 0f, Vector3 relativePosition = default) {
+    public void DisplayMessage(GameObject go, string message, float time = 0f, Vector3 relativePosition = default, float size = 1f, bool follow = false) {
         GameObject messageObject = Instantiate(MessagePrefab);
         CommunicationMessage msg = messageObject.GetComponent<CommunicationMessage>();
-        msg.FollowObject = go;
+
+        if (go == null)
+            msg.FollowObject = Camera.main.gameObject;
+        else {
+            msg.FollowObject = go;
+        }
+
+        msg.Follow = follow;
+        msg.Size = size;
+        msg.TerrainLayer = TerrainLayer;
         msg.DisappearTime = time;
         msg.Text = message;
         msg.RelativePosition = relativePosition;
--- Assets/Scripts/CommunicationMessage.cs
+++ Assets/Scripts/CommunicationMessage.cs
@@ -8,11 +8,14 @@ public class CommunicationMessage : MonoBehaviour
 {
     public bool FacePlayer = true;
     public GameObject FollowObject;
+    public bool Follow = false;
     public Vector3 RelativePosition;
     public float DisappearTime = 0f;
     public string Text;
+    public float Size;
     public TextMeshProUGUI Tmp;
     public Image img;
+    public LayerMask TerrainLayer; 
 
     private bool fade;
     private float startAlphaText;
@@ -32,12 +35,16 @@ public class CommunicationMessage : MonoBehaviour
         startAlphaImage = img.color.a;
 
         fadeTimer = CommunicationManager.Instance.DisappearTime;
+        transform.localScale = new Vector3(Size, Size, Size);
+        DoFollow();
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.position = FollowObject.transform.position + RelativePosition;
+        if (Follow)
+            DoFollow();
+
         Vector3 targetDirection = transform.position - Camera.main.transform.position;
         transform.rotation = Quaternion.LookRotation(Vector3.RotateTowards(transform.forward, targetDirection, 1f, 0f));
 
@@ -53,6 +60,29 @@ public class CommunicationMessage : MonoBehaviour
 
         if (Tmp.alpha <= 0f && img.color.a <= 0f)
             Destroy(gameObject);
+    }
+
+    private void DoFollow() {
+        transform.position = FollowObject.transform.position + RelativePosition;
+        RaycastHit hit;
+        float height = -100f;
+        for (int i = -1; i < 2; i++) {
+            if (Physics.Raycast(transform.position + Vector3.up * 100f + transform.forward * i * Size * 2.5f, Vector3.down, out hit, 200f, TerrainLayer)) {
+                if (hit.point.y > height)
+                    height = hit.point.y;
+            }
+        }
+        if (height > transform.position.y - Size) {
+            transform.position = new Vector3(transform.position.x, height + Size, transform.position.z);
+        }
+
+        Vector3 heading = transform.position - Camera.main.transform.position;
+        if(Physics.SphereCast(Camera.main.transform.position, 0.5f, heading.normalized, out hit, 1000f)) {
+            if (!hit.collider.CompareTag("Message")) {
+                Vector3 hd = Camera.main.transform.position - hit.point;
+                transform.position = hit.point + hd.normalized;
+            }
+        }
     }
 
     private void StartFade() {
