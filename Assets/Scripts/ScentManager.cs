f37d3890222c3c5aed4b3b54f9f96996f6047643
Daniel <dabergshoeff@gmail.com>
2020-03-06 10:58:22 +0100

    Added scent objects

---
+++ Assets/Scripts/ScentManager.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Sensiks.SDK.UnityLibrary;
+using Sensiks.SDK.Shared.SensiksDataTypes;
+using System;
+
+public class ScentManager : MonoBehaviour
+{
+    public static List<ScentObject> scentObjects;
+    private Dictionary<Scent, float> scentToStrength;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(ShowScent());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        SetScent();
+    }
+
+    private IEnumerator ShowScent() {
+        yield return new WaitForSeconds(1f);
+        foreach (Scent s in Enum.GetValues(typeof(Scent))) {
+            if (scentToStrength.ContainsKey(s))
+                Debug.Log(s + ": " + scentToStrength[s]);
+        }
+        StartCoroutine(ShowScent());
+    }
+
+    private void SetScent() {
+        scentToStrength = new Dictionary<Scent, float>();
+        foreach(ScentObject so in scentObjects) {
+            float sqrDistance = (so.transform.position - Camera.main.transform.position).sqrMagnitude;
+            if (sqrDistance > so.MaxDistance * so.MaxDistance)
+                continue;
+            float scentStrength = 1f - sqrDistance / (so.MaxDistance * so.MaxDistance);
+            scentStrength *= so.Strength;
+
+            if (scentToStrength.ContainsKey(so.ObjectScent))
+                scentToStrength[so.ObjectScent] += scentStrength;
+            else
+                scentToStrength.Add(so.ObjectScent, scentStrength);
+        }
+    }
+}
---
+++ Assets/Scripts/ScentObject.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Sensiks.SDK.Shared.SensiksDataTypes;
+
+public class ScentObject : MonoBehaviour
+{
+    public float Strength;
+    public Scent ObjectScent;
+    public float MaxDistance;
+
+    private bool scentOn = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (ScentManager.scentObjects == null)
+            ScentManager.scentObjects = new List<ScentObject>();
+
+        ScentManager.scentObjects.Add(this);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other) {
+        if (!other.CompareTag("Player"))
+            return;
+
+        scentOn = true;
+    }
+
+    private void OnTriggerExit(Collider other) {
+        if (!other.CompareTag("Player"))
+            return;
+
+        scentOn = false;
+    }
+}
