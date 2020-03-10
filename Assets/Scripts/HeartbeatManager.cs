d5599c60c409ae2b870de61906c9aa87e1189edc
Daniel <dabergshoeff@gmail.com>
2020-03-10 22:45:16 +0100

    Testing

---
+++ Assets/Scripts/HeartbeatManager.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Sensiks.SDK.Shared.SensiksDataTypes;
+using Sensiks.SDK.UnityLibrary;
+
+public class HeartbeatManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.Log(SensiksManager.Instance.CurrentPulseSensorIbiValue);
+    }
+}
