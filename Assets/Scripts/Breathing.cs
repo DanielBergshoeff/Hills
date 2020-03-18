bc31d24d8ffd8a5df024fa490e115a4e31121ce4
Daniel <dabergshoeff@gmail.com>
2020-03-18 11:52:58 +0100

    Simplistic breathing exercise

--- Assets/Scripts/Breathing.cs
+++ Assets/Scripts/Breathing.cs
@@ -11,15 +11,12 @@ public class Breathing : MonoBehaviour
     [SerializeField] private float holdBreathTime = 3.0f;
     [SerializeField] private float breatheOutTime = 3.0f;
     [SerializeField] private float waitForNewBreath = 1.0f;
-      
-    private float timer = 0f;
-    private bool breatheIn = false;
 
     // Start is called before the first frame update
     void Start()
     {
         vfxGraph = GetComponent<VisualEffect>();
-        Invoke("BreatheOut", 0f);
+        Invoke("BreatheIn", 0f);
     }
 
     private void BreatheIn() {
@@ -34,7 +31,7 @@ public class Breathing : MonoBehaviour
     }
 
     private void BreatheOut() {
-        vfxGraph.SetFloat("Direction", 0f);
+        vfxGraph.SetFloat("Direction", -1f);
         vfxGraph.SetFloat("Speed", 1f / breatheOutTime);
         Invoke("WaitForNewBreath", breatheOutTime);
     }
@@ -42,16 +39,5 @@ public class Breathing : MonoBehaviour
     private void WaitForNewBreath() {
         vfxGraph.SetFloat("Speed", 0f);
         Invoke("BreatheIn", waitForNewBreath);
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        if(timer < breatheOutTime && breatheIn) {
-            timer += Time.deltaTime;
-            if (timer >= 0f) {
-                vfxGraph.SetFloat("Speed", 0f);
-            }
-        }
     }
 }
